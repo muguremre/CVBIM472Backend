d740da2e4ee149d3c4f9369b04b29b96ba0b4801
Muhammed Uğur EMRE <muhammeduguremre@gmail.com>
2024-06-04 05:56:55 +0300

    backend finish

---
+++ DataAccess/MuguremrecvContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using muguremreCVBackend.Entities;
+
+namespace muguremreCVBackend.DataAccess
+{
+    public class MuguremrecvContext : DbContext
+    {
+        public MuguremrecvContext(DbContextOptions<MuguremrecvContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ DataAccess/RegisterRepository.cs
@@ -0,0 +1,28 @@
+﻿using muguremreCVBackend.Entities;
+using System.Threading.Tasks;
+
+namespace muguremreCVBackend.DataAccess
+{
+    public interface IRegisterRepository
+    {
+        Task<int> AddRegisterAsync(User user);
+    }
+
+    public class RegisterRepository : IRegisterRepository
+    {
+        private readonly MuguremrecvContext _context;
+
+        public RegisterRepository(MuguremrecvContext context)
+        {
+            _context = context; // MuguremrecvContext enjekte ediliyor
+        }
+
+        public async Task<int> AddRegisterAsync(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return user.Id;
+        }
+    }
+
+}
